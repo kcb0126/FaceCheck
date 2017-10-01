62413774a6b3e671199203da099d838ec2e66958
kcb0126 <30620567+kcb0126@users.noreply.github.com>
2017-10-02 00:40:26 +0800

    UserManageList was added to PageOfUserManage.

--- FaceCheck/FaceCheck/PageOfUserManage.cpp
+++ FaceCheck/FaceCheck/PageOfUserManage.cpp
@@ -28,7 +28,36 @@ void CPageOfUserManage::DoDataExchange(CDataExchange* pDX)
 
 
 BEGIN_MESSAGE_MAP(CPageOfUserManage, CPropertyPage)
+	ON_WM_SIZE()
 END_MESSAGE_MAP()
 
 
 // CPageOfUserManage message handlers
+
+
+BOOL CPageOfUserManage::OnInitDialog()
+{
+	CPropertyPage::OnInitDialog();
+
+	m_userManageList.Create(CUserManageList::IDD, this);
+	m_userManageList.ShowWindow(SW_SHOW);
+	m_userManageList.UpdateWindow();
+
+	return TRUE;  // return TRUE unless you set the focus to a control
+	// EXCEPTION: OCX Property Pages should return FALSE
+}
+
+
+void CPageOfUserManage::OnSize(UINT nType, int cx, int cy)
+{
+	CPropertyPage::OnSize(nType, cx, cy);
+
+	CRect rect;
+	GetClientRect(&rect);
+
+	if (m_userManageList.m_hWnd != NULL)
+	{
+		m_userManageList.SetWindowPos(NULL, 0, 0, rect.Width(), rect.Height(), SWP_NOZORDER);
+		m_userManageList.UpdateWindow();
+	}
+}
--- FaceCheck/FaceCheck/PageOfUserManage.h
+++ FaceCheck/FaceCheck/PageOfUserManage.h
@@ -1,5 +1,5 @@
 #pragma once
-
+#include "UserManageList.h"
 
 // CPageOfUserManage dialog
 
@@ -18,4 +18,9 @@ protected:
 	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV support
 
 	DECLARE_MESSAGE_MAP()
+
+public:
+	CUserManageList m_userManageList;
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSize(UINT nType, int cx, int cy);
 };
