8b91af934d1cfbe73a657494406abd5f162e8942
kcb0126 <30620567+kcb0126@users.noreply.github.com>
2017-10-01 18:14:18 +0800

    CellOfPersonHistory.h was included in ListOfPersonHistory.h

--- FaceCheck/FaceCheck/ListOfPersonHistory.cpp
+++ FaceCheck/FaceCheck/ListOfPersonHistory.cpp
@@ -1,6 +1,9 @@
 #include "stdafx.h"
 #include "ListOfPersonHistory.h"
+#include "CellOfPersonHistory.h"
 
+
+IMPLEMENT_DYNCREATE(CListOfPersonHistory, CScrollView)
 
 CListOfPersonHistory::CListOfPersonHistory()
 {
@@ -9,4 +12,18 @@ CListOfPersonHistory::CListOfPersonHistory()
 
 CListOfPersonHistory::~CListOfPersonHistory()
 {
+}
+
+int CListOfPersonHistory::GetCellCount()
+{
+	return m_nCount;
+}
+
+CWnd* CListOfPersonHistory::GetCellAt(int nIndex)
+{
+	CCellOfPersonHistory* pCell;
+	pCell = new CCellOfPersonHistory;
+	pCell->Create(CCellOfPersonHistory::IDD, this);
+	pCell->ShowWindow(SW_SHOW);
+	return pCell;
 }
--- FaceCheck/FaceCheck/ListOfPersonHistory.h
+++ FaceCheck/FaceCheck/ListOfPersonHistory.h
@@ -3,8 +3,15 @@
 class CListOfPersonHistory :
 	public CCustomListView
 {
+	DECLARE_DYNCREATE(CListOfPersonHistory)
+
 public:
 	CListOfPersonHistory();
 	~CListOfPersonHistory();
+
+public:
+	int m_nCount = 5;
+	virtual int GetCellCount();
+	virtual CWnd* GetCellAt(int nIndex);
 };
 
