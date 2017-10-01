b4ea814f46b02d7d664bc64321c4e3184562870d
kcb0126 <30620567+kcb0126@users.noreply.github.com>
2017-10-01 18:41:30 +0800

    PersonHistoryList was inserted in PageOfPersonHistory.

--- FaceCheck/FaceCheck/PageOfPersonHistory.cpp
+++ FaceCheck/FaceCheck/PageOfPersonHistory.cpp
@@ -28,7 +28,36 @@ void CPageOfPersonHistory::DoDataExchange(CDataExchange* pDX)
 
 
 BEGIN_MESSAGE_MAP(CPageOfPersonHistory, CPropertyPage)
+	ON_WM_SIZE()
 END_MESSAGE_MAP()
 
 
 // CPageOfPersonHistory message handlers
+
+
+BOOL CPageOfPersonHistory::OnInitDialog()
+{
+	CPropertyPage::OnInitDialog();
+
+	m_personHistoryList.Create(CPersonHistoryList::IDD, this);
+	m_personHistoryList.ShowWindow(SW_SHOW);
+	m_personHistoryList.UpdateWindow();
+
+	return TRUE;  // return TRUE unless you set the focus to a control
+	// EXCEPTION: OCX Property Pages should return FALSE
+}
+
+
+void CPageOfPersonHistory::OnSize(UINT nType, int cx, int cy)
+{
+	CPropertyPage::OnSize(nType, cx, cy);
+
+	CRect rect;
+	GetClientRect(&rect);
+
+	if (m_personHistoryList.m_hWnd != NULL)
+	{
+		m_personHistoryList.SetWindowPos(NULL, 0, 0, rect.Width(), rect.Height(), SWP_NOZORDER);
+		m_personHistoryList.UpdateWindow();
+	}
+}
--- FaceCheck/FaceCheck/PageOfPersonHistory.h
+++ FaceCheck/FaceCheck/PageOfPersonHistory.h
@@ -1,4 +1,5 @@
 #pragma once
+#include "PersonHistoryList.h"
 
 
 // CPageOfPersonHistory dialog
@@ -18,4 +19,9 @@ protected:
 	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV support
 
 	DECLARE_MESSAGE_MAP()
+
+public:
+	CPersonHistoryList m_personHistoryList;
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSize(UINT nType, int cx, int cy);
 };
