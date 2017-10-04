c29e4a4961fd21066ed753e3fe527f0c90cddb98
kcb0126 <30620567+kcb0126@users.noreply.github.com>
2017-10-05 00:44:00 +0800

    CellOfPersonManage was completed, I think.

--- FaceCheck/FaceCheck/CellOfPersonManage.cpp
+++ FaceCheck/FaceCheck/CellOfPersonManage.cpp
@@ -24,11 +24,62 @@ CCellOfPersonManage::~CCellOfPersonManage()
 void CCellOfPersonManage::DoDataExchange(CDataExchange* pDX)
 {
 	CPropertyPage::DoDataExchange(pDX);
+	DDX_Control(pDX, IDC_STATIC_NAME, m_lblName);
+	DDX_Control(pDX, IDC_STATIC_NICKNAME1, m_lblNickname1);
+	DDX_Control(pDX, IDC_STATIC_NICKNAME, m_lblNickname);
+	DDX_Control(pDX, IDC_STATIC_GENDER, m_lblGender);
+	DDX_Control(pDX, IDC_STATIC_BIRTHDAY1, m_lblBirthday1);
+	DDX_Control(pDX, IDC_STATIC_BIRTHDAY, m_lblBirthday);
+	DDX_Control(pDX, IDC_STATIC_GUESTTYPE1, m_lblGuesttype1);
+	DDX_Control(pDX, IDC_STATIC_GUESTTYPE, m_lblGuesttype);
+	DDX_Control(pDX, IDC_STATIC_SAFETYTYPE1, m_lblSafetytype1);
+	DDX_Control(pDX, IDC_STATIC_SAFETYTYPE, m_lblSafetytype);
+	DDX_Control(pDX, IDC_STATIC_BLOCKED, m_lblBlocked);
 }
 
 
 BEGIN_MESSAGE_MAP(CCellOfPersonManage, CPropertyPage)
+	ON_WM_ERASEBKGND()
 END_MESSAGE_MAP()
 
 
 // CCellOfPersonManage message handlers
+
+
+BOOL CCellOfPersonManage::OnEraseBkgnd(CDC* pDC)
+{
+	CPropertyPage::OnEraseBkgnd(pDC);
+
+	CRect rect;
+	GetClientRect(&rect);
+	CDC dc;
+	dc.CreateCompatibleDC(pDC);
+	CBitmap* pOldBitmap;
+
+	CBitmap bmpLightGray;
+	bmpLightGray.LoadBitmap(IDB_LIGHTGRAY);
+	pOldBitmap = dc.SelectObject(&bmpLightGray);
+	pDC->StretchBlt(0, 0, rect.Width(), rect.Height(), &dc, 0, 0, 100, 100, SRCCOPY);
+	return TRUE;
+}
+
+
+BOOL CCellOfPersonManage::OnInitDialog()
+{
+	CPropertyPage::OnInitDialog();
+
+	m_lblName.SetTextColor(RGB(255, 255, 255));
+	m_lblNickname1.SetTextColor(RGB(255, 255, 255));
+	m_lblNickname.SetTextColor(RGB(255, 255, 255));
+	m_lblGender.SetTextColor(RGB(255, 255, 255));
+	m_lblBirthday1.SetTextColor(RGB(255, 255, 255));
+	m_lblBirthday.SetTextColor(RGB(255, 255, 255));
+	m_lblGuesttype1.SetTextColor(RGB(255, 255, 255));
+	m_lblGuesttype.SetTextColor(RGB(255, 255, 255));
+	m_lblSafetytype1.SetTextColor(RGB(255, 255, 255));
+	m_lblSafetytype.SetTextColor(RGB(255, 255, 255));
+	m_lblBlocked.SetTextColor(RGB(255, 255, 255));
+
+	return TRUE;  // return TRUE unless you set the focus to a control
+	// EXCEPTION: OCX Property Pages should return FALSE
+}
--- FaceCheck/FaceCheck/CellOfPersonManage.h
+++ FaceCheck/FaceCheck/CellOfPersonManage.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "resource.h"
+#include "LabelTrans.h"
 
 // CCellOfPersonManage dialog
 
@@ -18,4 +19,18 @@ protected:
 	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV support
 
 	DECLARE_MESSAGE_MAP()
+public:
+	CLabelTrans m_lblName;
+	CLabelTrans m_lblNickname1;
+	CLabelTrans m_lblNickname;
+	CLabelTrans m_lblGender;
+	CLabelTrans m_lblBirthday1;
+	CLabelTrans m_lblBirthday;
+	CLabelTrans m_lblGuesttype1;
+	CLabelTrans m_lblGuesttype;
+	CLabelTrans m_lblSafetytype1;
+	CLabelTrans m_lblSafetytype;
+	CLabelTrans m_lblBlocked;
+	afx_msg BOOL OnEraseBkgnd(CDC* pDC);
+	virtual BOOL OnInitDialog();
 };
