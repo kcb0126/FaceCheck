75faad6770639f297e398bad4e128bd8c9967b70
kcb0126 <30620567+kcb0126@users.noreply.github.com>
2017-10-05 23:11:58 +0800

    ListOfPersonHistory was completed.

--- FaceCheck/FaceCheck/ListOfPersonHistory.cpp
+++ FaceCheck/FaceCheck/ListOfPersonHistory.cpp
@@ -24,6 +24,35 @@ CWnd* CListOfPersonHistory::GetCellAt(int nIndex)
 	CCellOfPersonHistory* pCell;
 	pCell = new CCellOfPersonHistory;
 	pCell->Create(CCellOfPersonHistory::IDD, this);
+	pCell->m_staticPhoto.SetBitmap(m_cellsData[nIndex].hPhoto);
+	pCell->m_strTime = m_cellsData[nIndex].strTime;
+	pCell->m_strName = m_cellsData[nIndex].strName;
+	pCell->m_strGender = m_cellsData[nIndex].strGender;
+	pCell->m_strAge = m_cellsData[nIndex].strAge;
+	pCell->m_strPlace = m_cellsData[nIndex].strPlace;
+	pCell->m_strView = m_cellsData[nIndex].strView;
+	pCell->m_strComment = m_cellsData[nIndex].strComment;
+	pCell->UpdateData(FALSE);
 	pCell->ShowWindow(SW_SHOW);
 	return pCell;
+}BEGIN_MESSAGE_MAP(CListOfPersonHistory, CCustomListView)
+ON_WM_ERASEBKGND()
+END_MESSAGE_MAP()
+
+
+BOOL CListOfPersonHistory::OnEraseBkgnd(CDC* pDC)
+{
+	CCustomListView::OnEraseBkgnd(pDC);
+
+	CRect rect;
+	GetClientRect(&rect);
+	CDC dc;
+	dc.CreateCompatibleDC(pDC);
+	CBitmap* pOldBitmap;
+
+	CBitmap bmpLightGray;
+	bmpLightGray.LoadBitmap(IDB_LIGHTGRAY);
+	pOldBitmap = dc.SelectObject(&bmpLightGray);
+	pDC->StretchBlt(0, 0, rect.Width(), rect.Height(), &dc, 0, 0, 100, 100, SRCCOPY);
+	return TRUE;
 }
--- FaceCheck/FaceCheck/ListOfPersonHistory.h
+++ FaceCheck/FaceCheck/ListOfPersonHistory.h
@@ -1,5 +1,18 @@
 #pragma once
 #include "CustomListView.h"
+
+struct tagPersonHistoryCell
+{
+	HBITMAP hPhoto;
+	CString strTime;
+	CString strName;
+	CString strGender;
+	CString strAge;
+	CString strPlace;
+	CString strView;
+	CString strComment;
+};
+
 class CListOfPersonHistory :
 	public CCustomListView
 {
@@ -10,8 +23,11 @@ public:
 	~CListOfPersonHistory();
 
 public:
-	int m_nCount = 5;
+	int m_nCount = 0;
 	virtual int GetCellCount();
 	virtual CWnd* GetCellAt(int nIndex);
+	std::vector<tagPersonHistoryCell> m_cellsData;
+	DECLARE_MESSAGE_MAP()
+	afx_msg BOOL OnEraseBkgnd(CDC* pDC);
 };
 
