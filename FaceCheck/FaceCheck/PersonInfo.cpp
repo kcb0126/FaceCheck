1c1a4c10a6a6f4836aa02ee1a86e3a4da59e8e3b
kcb0126 <30620567+kcb0126@users.noreply.github.com>
2017-10-05 13:20:46 +0800

    Controls in PersonInfo dialog has their own ID, and some member variables..

--- FaceCheck/FaceCheck/PersonInfo.cpp
+++ FaceCheck/FaceCheck/PersonInfo.cpp
@@ -13,6 +13,19 @@ IMPLEMENT_DYNAMIC(CPersonInfo, CPropertyPage)
 
 CPersonInfo::CPersonInfo()
 	: CPropertyPage(CPersonInfo::IDD)
+	, m_nGender(0)
+	, m_nPlayerLevel(0)
+	, m_nSecurityLevel(0)
+	, m_dateBirthday(0)
+	, m_dateBlockedWhen(0)
+	, m_dateTo(0)
+	, m_strFirstName(_T(""))
+	, m_strLastName(_T(""))
+	, m_strNickName(_T(""))
+	, m_strBlockedReason(_T(""))
+	, m_strBlockedPlace(_T(""))
+	, m_nBlockType(0)
+	, m_nBlocked(0)
 {
 
 }
@@ -24,6 +37,21 @@ CPersonInfo::~CPersonInfo()
 void CPersonInfo::DoDataExchange(CDataExchange* pDX)
 {
 	CPropertyPage::DoDataExchange(pDX);
+	DDX_CBIndex(pDX, IDC_COMBO_GENDER, m_nGender);
+	DDX_CBIndex(pDX, IDC_COMBO_GUESTTYPE, m_nPlayerLevel);
+	DDX_CBIndex(pDX, IDC_COMBO_SAFETYTYPE, m_nSecurityLevel);
+	DDX_DateTimeCtrl(pDX, IDC_DATETIMEPICKER_BIRTHDAY, m_dateBirthday);
+	DDX_DateTimeCtrl(pDX, IDC_DATETIMEPICKER_FROM, m_dateBlockedWhen);
+	DDX_DateTimeCtrl(pDX, IDC_DATETIMEPICKER_TO, m_dateTo);
+	DDX_Text(pDX, IDC_EDIT_FIRSTNAME, m_strFirstName);
+	DDX_Control(pDX, IDC_EDIT_INFOMATION, m_strInformation);
+	DDX_Text(pDX, IDC_EDIT_LASTNAME, m_strLastName);
+	DDX_Text(pDX, IDC_EDIT_NICKNAME, m_strNickName);
+	DDX_Text(pDX, IDC_EDIT_REASON, m_strBlockedReason);
+	DDX_Text(pDX, IDC_EDIT_WHERE, m_strBlockedPlace);
+	DDX_Radio(pDX, IDC_RADIO_LOCAL, m_nBlockType);
+	DDX_Radio(pDX, IDC_RADIO_UNBLOCKED, m_nBlocked);
+	DDX_Control(pDX, IDC_STATIC_PHOTO, m_staticPhoto);
 }
 
 
--- FaceCheck/FaceCheck/PersonInfo.h
+++ FaceCheck/FaceCheck/PersonInfo.h
@@ -1,4 +1,6 @@
 #pragma once
+#include "atltime.h"
+#include "afxwin.h"
 
 
 // CPersonInfo dialog
@@ -18,4 +20,20 @@ protected:
 	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV support
 
 	DECLARE_MESSAGE_MAP()
+public:
+	int m_nGender;
+	int m_nPlayerLevel;
+	int m_nSecurityLevel;
+	CTime m_dateBirthday;
+	CTime m_dateBlockedWhen;
+	CTime m_dateTo;
+	CString m_strFirstName;
+	CEdit m_strInformation;
+	CString m_strLastName;
+	CString m_strNickName;
+	CString m_strBlockedReason;
+	CString m_strBlockedPlace;
+	int m_nBlockType;
+	int m_nBlocked;
+	CStatic m_staticPhoto;
 };
