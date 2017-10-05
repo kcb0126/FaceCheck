af4d62127a93f11a38a97d9e1b7392c40a25dec1
kcb0126 <30620567+kcb0126@users.noreply.github.com>
2017-10-06 02:06:37 +0800

    CellOfUserManage was completed.

--- FaceCheck/FaceCheck/CellOfUserManage.cpp
+++ FaceCheck/FaceCheck/CellOfUserManage.cpp
@@ -13,6 +13,11 @@ IMPLEMENT_DYNAMIC(CCellOfUserManage, CPropertyPage)
 
 CCellOfUserManage::CCellOfUserManage()
 	: CPropertyPage(CCellOfUserManage::IDD)
+	, m_strUsername(_T(""))
+	, m_strPrivilege(_T(""))
+	, m_strCreator(_T(""))
+	, m_strPhoneNo(_T(""))
+	, m_strBlocked(_T(""))
 {
 
 }
@@ -24,11 +29,59 @@ CCellOfUserManage::~CCellOfUserManage()
 void CCellOfUserManage::DoDataExchange(CDataExchange* pDX)
 {
 	CPropertyPage::DoDataExchange(pDX);
+	DDX_Text(pDX, IDC_STATIC_USERNAME, m_strUsername);
+	DDX_Text(pDX, IDC_STATIC_PRIVILEGE, m_strPrivilege);
+	DDX_Text(pDX, IDC_STATIC_CREATOR, m_strCreator);
+	DDX_Text(pDX, IDC_STATIC_PHONENO, m_strPhoneNo);
+	DDX_Text(pDX, IDC_STATIC_BLOCKED, m_strBlocked);
+	DDX_Control(pDX, IDC_STATIC_USERNAME, m_staticUsername);
+	DDX_Control(pDX, IDC_STATIC_PRIVILEGE, m_staticPrivilege);
+	DDX_Control(pDX, IDC_STATIC_CREATEDBY, m_staticCreatedBy);
+	DDX_Control(pDX, IDC_STATIC_CREATOR, m_staticCreator);
+	DDX_Control(pDX, IDC_STATIC_PHONENO1, m_staticPhoneNo1);
+	DDX_Control(pDX, IDC_STATIC_PHONENO, m_staticPhoneNo);
+	DDX_Control(pDX, IDC_STATIC_BLOCKED, m_staticBlocked);
 }
 
 
 BEGIN_MESSAGE_MAP(CCellOfUserManage, CPropertyPage)
+	ON_WM_ERASEBKGND()
 END_MESSAGE_MAP()
 
 
 // CCellOfUserManage message handlers
+
+
+BOOL CCellOfUserManage::OnInitDialog()
+{
+	CPropertyPage::OnInitDialog();
+
+	m_staticUsername.SetTextColor(RGB(255, 255, 255));
+	m_staticPrivilege.SetTextColor(RGB(255, 255, 255));
+	m_staticCreatedBy.SetTextColor(RGB(255, 255, 255));
+	m_staticCreator.SetTextColor(RGB(255, 255, 255));
+	m_staticPhoneNo1.SetTextColor(RGB(255, 255, 255));
+	m_staticPhoneNo.SetTextColor(RGB(255, 255, 255));
+	m_staticBlocked.SetTextColor(RGB(255, 255, 255));
+
+	return TRUE;  // return TRUE unless you set the focus to a control
+	// EXCEPTION: OCX Property Pages should return FALSE
+}
+
+
+BOOL CCellOfUserManage::OnEraseBkgnd(CDC* pDC)
+{
+	CPropertyPage::OnEraseBkgnd(pDC);
+
+	CRect rect;
+	GetClientRect(&rect);
+	CDC dc;
+	dc.CreateCompatibleDC(pDC);
+	CBitmap* pOldBitmap;
+
+	CBitmap bmpLightGray;
+	bmpLightGray.LoadBitmap(IDB_LIGHTGRAY);
+	pOldBitmap = dc.SelectObject(&bmpLightGray);
+	pDC->StretchBlt(0, 0, rect.Width(), rect.Height(), &dc, 0, 0, 100, 100, SRCCOPY);
+	return TRUE;
+}
--- FaceCheck/FaceCheck/CellOfUserManage.h
+++ FaceCheck/FaceCheck/CellOfUserManage.h
@@ -1,6 +1,7 @@
 #pragma once
 #include "Resource.h"
-
+#include "afxwin.h"
+#include "LabelTrans.h"
 
 // CCellOfUserManage dialog
 
@@ -19,4 +20,19 @@ protected:
 	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV support
 
 	DECLARE_MESSAGE_MAP()
+public:
+	virtual BOOL OnInitDialog();
+	afx_msg BOOL OnEraseBkgnd(CDC* pDC);
+	CString m_strUsername;
+	CString m_strPrivilege;
+	CString m_strCreator;
+	CString m_strPhoneNo;
+	CString m_strBlocked;
+	CLabelTrans m_staticUsername;
+	CLabelTrans m_staticPrivilege;
+	CLabelTrans m_staticCreatedBy;
+	CLabelTrans m_staticCreator;
+	CLabelTrans m_staticPhoneNo1;
+	CLabelTrans m_staticPhoneNo;
+	CLabelTrans m_staticBlocked;
 };
