cc971798356c838073ce999faa640a2d3550a63d
kcb0126 <30620567+kcb0126@users.noreply.github.com>
2017-10-06 11:34:29 +0800

    ListOfUserHistory was completed.

--- FaceCheck/FaceCheck/ListOfUserHistory.cpp
+++ FaceCheck/FaceCheck/ListOfUserHistory.cpp
@@ -24,6 +24,31 @@ CWnd* CListOfUserHistory::GetCellAt(int nIndex)
 	CCellOfUserHistory* pCell;
 	pCell = new CCellOfUserHistory;
 	pCell->Create(CCellOfUserHistory::IDD, this);
+	pCell->m_strUsername = m_cellsData[nIndex].strUsername;
+	pCell->m_strAction = m_cellsData[nIndex].strAction;
+	pCell->m_strTime = m_cellsData[nIndex].strTime;
+	pCell->m_strContent = m_cellsData[nIndex].strContent;
+	pCell->UpdateData(FALSE);
 	pCell->ShowWindow(SW_SHOW);
 	return pCell;
+}BEGIN_MESSAGE_MAP(CListOfUserHistory, CCustomListView)
+ON_WM_ERASEBKGND()
+END_MESSAGE_MAP()
+
+
+BOOL CListOfUserHistory::OnEraseBkgnd(CDC* pDC)
+{
+	CCustomListView::OnEraseBkgnd(pDC);
+
+	CRect rect;
+	GetClientRect(&rect);
+	CDC dc;
+	dc.CreateCompatibleDC(pDC);
+	CBitmap* pOldBitmap;
+
+	CBitmap bmpLightGray;
+	bmpLightGray.LoadBitmap(IDB_LIGHTGRAY);
+	pOldBitmap = dc.SelectObject(&bmpLightGray);
+	pDC->StretchBlt(0, 0, rect.Width(), rect.Height(), &dc, 0, 0, 100, 100, SRCCOPY);
+	return TRUE;
 }
--- FaceCheck/FaceCheck/ListOfUserHistory.h
+++ FaceCheck/FaceCheck/ListOfUserHistory.h
@@ -1,5 +1,14 @@
 #pragma once
 #include "CustomListView.h"
+
+struct tagUserHistoryCell
+{
+	CString strUsername;
+	CString strAction;
+	CString strTime;
+	CString strContent;
+};
+
 class CListOfUserHistory :
 	public CCustomListView
 {
@@ -10,8 +19,11 @@ public:
 	~CListOfUserHistory();
 
 public:
-	int m_nCount = 5;
+	int m_nCount = 0;
 	virtual int GetCellCount();
 	virtual CWnd* GetCellAt(int nIndex);
+	std::vector<tagUserHistoryCell> m_cellsData;
+	DECLARE_MESSAGE_MAP()
+	afx_msg BOOL OnEraseBkgnd(CDC* pDC);
 };
 
